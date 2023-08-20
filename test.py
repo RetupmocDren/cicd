c683c530425f9ec7b08b40400f48d00e5a8ff528
RetupmocDren <sonehc@yahoo.de>
2023-08-20 09:17:14 +0200

    updated test.py

--- test.py
+++ test.py
@@ -4,6 +4,29 @@ DOCUMENTATION:
 """
 
 import app
+import sys
 
-# test
-app.main("World")
+
+# tests
+def test():
+    """
+    simple smoke test... does it work?
+    """
+    try:
+        # test with arg
+        print("Test with arg: ")
+        app.main("World")
+
+        # test with no arg
+        print("\nTest without arg: ")
+        app.main()
+    except Exception:
+        print(sys.exc_info())
+        return 0
+    
+    return 1
+
+#-------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    test()
